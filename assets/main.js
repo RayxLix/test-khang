21c4c1447490407a22cd5f828f18cca57d085fab
Nguyen Huu Huy <huytranduong78@gmail.com>
2023-03-09 17:26:55 +0700

    all

---
+++ assets/main.js
@@ -0,0 +1,7 @@
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  document.getElementById("linkzalo").href = "https://zalo.me/SĐT_Của_Bạn";
+}
